15397442f52105bbc6e8d2b7aa1c5f8e06473d43
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2024-02-13 21:50:22 +0300

    Add files via upload

---
+++ BlazorApp1/BlazorApp1/Pages/Counter2Base.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages
+{
+    public class Counter2Base:ComponentBase
+    {
+        protected int currentCount = 0;//varsayilan değer atanir fakat diğer sayfalardan erişip farklı değerle başlatılabilir
+
+        protected void IncrementCount()//sadece miras alınan erişme sağlar
+        {
+            currentCount++;
+        }
+    }
+}
