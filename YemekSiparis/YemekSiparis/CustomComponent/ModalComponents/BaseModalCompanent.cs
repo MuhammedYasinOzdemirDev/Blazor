7744145687081432e2a93275beb5a01c914a615f
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2024-02-13 21:52:46 +0300

    Add files via upload

---
+++ YemekSiparis/YemekSiparis/CustomComponent/ModalComponents/BaseModalCompanent.cs
@@ -0,0 +1,35 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+
+namespace YemekSiparis.CustomComponent.ModalComponents
+{
+    public class BaseModalCompanent:ComponentBase
+    {
+        [Parameter]
+        public string Message { get; set; }
+        [Parameter]
+        public string OkText { get; set; }
+        [Parameter]
+        public string CancelText { get; set; }
+        [Parameter]
+        public string YesText { get; set; }
+        [Parameter]
+        public string NoText { get; set; }
+        [CascadingParameter]
+        BlazoredModalInstance ModalInstance { get; set; }//Modal özelliklerini değiştirmek tusa basıldımı çıkma vs işlemleri yapar
+        
+        protected async Task ConfirmClicked()//Sadece miras verilen classını kullanabildiği Tamam basınca cıkan metod yazılır
+        {
+            await ModalInstance.CloseAsync(ModalResult.Ok());
+
+        }
+        protected async Task CancelClicked()//Sadece miras verilen classını kullanabildiği Tamam basınca cıkan metod yazılır
+        {
+            await ModalInstance.CloseAsync(ModalResult.Cancel());
+
+        }
+
+    }
+}
---
+++ YemekSiparis/YemekSiparis/Utils/ModalManager.cs
@@ -0,0 +1,39 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using System.Reflection;
+using YemekSiparis.CustomComponent.ModalComponents;
+
+namespace YemekSiparis.Utils
+{
+    public class ModalManager
+    {
+        private readonly IModalService modalService;
+        public ModalManager(IModalService modalService)
+        {
+            this.modalService = modalService;
+        }
+        public async Task ShowMessageAsync(String title,String Message,int duration=0)
+        {
+
+            ModalParameters mParams = new ModalParameters();
+            mParams.Add("Message", Message);
+            var modalRef=modalService.Show<ShowMessagePopupComponent>(title, mParams);
+            if (duration > 0)
+            {
+                await Task.Delay(duration);//girilen ms kadar bekletir kapatir
+                modalRef.Close();
+            }
+            await modalRef.Result;//modal dan donen değeri alir
+        }
+        public async Task<bool> ConfirmationAsync(String title, String Message)
+        {
+
+            ModalParameters mParams = new ModalParameters();
+            mParams.Add("Message", "Cevabiniz Nedir?");
+            var modalRef = modalService.Show<ConfirmationPopupComponent>(title,mParams);
+           
+            var ModalResult= modalRef.Result;//modal dan donen değeri alir
+            return !ModalResult.IsCanceled;//onay mi red mi gonderir
+        }
+    }
+}
